8b669a15397ca91d9b3c4628a53caaa6738492c1
abhishek <abhishek10271@gmail.com>
2022-07-23 15:49:44 +0530

    renamed GitTest.java to Main.java

--- Message.java
+++ Message.java
@@ -1,5 +1,5 @@
 public class Message {
-    public static void main(String[] args) {
+    public static void printMessage() {
         System.out.print("This is a simple text message");
     }
 }
--- TextMessage.java
+++ TextMessage.java
@@ -1,5 +1,5 @@
 public class TextMessage {
-    public static void main(String[] args) {
+    public static void printTextMessage() {
         System.out.println("It is a simple text message");
     }
 }
